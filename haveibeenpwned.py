6d779c73e55b6dd286217025100979ee4c012001
Raphael Berch <berchraphael@googlemail.com>
2019-04-24 10:00:31 +0200

    added inputvalidation, logging and api-header

--- haveibeenpwned.py
+++ haveibeenpwned.py
@@ -8,6 +8,21 @@ import getpass
 import re
 import argparse
 import json
+import logging
+
+
+
+headers = {
+    'User-Agent': 'python-hibpwned-checker'
+}
+
+logger = logging.getLogger(__name__)
+
+# Uncomment next line for debugging
+#logging.basicConfig(level=logging.DEBUG)
+
+# Response codes can be found in the api description https://haveibeenpwned.com/API/v2 
+response_code = {200 : "Ok — everything worked and there\'s a string array of pwned sites for the account", 400 : "Bad request — the account does not comply with an acceptable format (i.e. it\'s an empty string)", 403 : "Forbidden — no user agent has been specified in the request", 404 : "Not found — the account could not be found and has therefore not been pwned", 429 : "Too many requests — the rate limit has been exceeded"}
 
 
 class color:
@@ -21,10 +36,11 @@ def checkEmail(numberEmail):
     for i in range(int(numberEmail)):
         email = input("Enter your email: ")
         try:
-             r = requests.get('https://haveibeenpwned.com/api/v2/breachedaccount/' + email)
+             r = requests.get('https://haveibeenpwned.com/api/v2/breachedaccount/' + email, headers=headers)
         except Exception as error:
-            print(color.WARNING + 'ERROR', "Can\'t connect to database")
+            logger.error(color.WARNING + "Error - Can\'t connect to database:\n {0}".format(error) + color.ENDC)
         else:
+            logger.debug(response_code[r.status_code])
             if r:
                  dic = json.loads(r.text)
                  print(color.WARNING+"\nYour email has been found in following leaks:\n")
@@ -39,7 +55,7 @@ def checkPassword(numberPassword):
         try:
             password = getpass.getpass()
         except Exception as error:
-            print('ERROR', error)
+            logger.error(color.WARNING + 'ERROR - {0}'.format(error) + color.ENDC)
         else:
             print('Searching for password...')
 
@@ -49,7 +65,7 @@ def checkPassword(numberPassword):
         try:
              r = requests.get('https://api.pwnedpasswords.com/range/'+ firstFive)
         except Exception as error:
-            print(color.WARNING + 'ERROR', '- Can not connect to API!')
+            logger.error(color.WARNING + 'ERROR - Can\'t connect to API!:\n{0}'.format(error) + color.ENDC)
         else:
             s = re.search(lastChars+".*",r.text)
             if s:
@@ -59,16 +75,24 @@ def checkPassword(numberPassword):
             else:
                  print(color.OKGREEN + 'Password not found!'+color.ENDC)
 
+def isInt(query):
+    while True:
+        try:
+             val = int(input("How many {0} do you want to check?: ".format(query)))
+        except ValueError:
+             print("That's not an int!")
+             continue
+        return val
 
 
 def main():
     print("This program can check if credentials are listed in the database off Troy Hunt (pwnedpasswords.com)\n\n")
     while True:
         print("First we will check if your email is affected")
-        numberEmail = input("How many email adresses do you want to check: ")
+        numberEmail = isInt("email adresses")
         checkEmail(numberEmail)
         print("\nNow lets check if your password is unsafe and occurs in the database\nThe passwords you insert are hashed and only a certain part of the hash is sent to the API")
-        numberPassword = input("How many passwords do you want to check: ")
+        numberPassword = isInt("passwords")
         checkPassword(numberPassword)
         answer = input("Do you want to continue?:")
         if answer.lower().startswith("y"):
